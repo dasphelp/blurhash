1387631a490b515cbd0a1e4ac16d08312a509acc
dasphelp <95512203+dasphelp@users.noreply.github.com>
2023-06-03 20:25:42 -0700

    Update fuzz.sh

--- fuzz.sh
+++ fuzz.sh
@@ -1,2 +1,12 @@
-file= echo "$1"
+#!/bin/bash
+
+# Read the contents of the file into a variable
+file_contents=$(cat "$1")
+
+# Call another program with the file contents as a string
+/path/to/another_program "$file_contents"f
+
+
+
+ile= echo "$1"
 ./blurhash_decoder "$file" 32 32 decoded_output.png
